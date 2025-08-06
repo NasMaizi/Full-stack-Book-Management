6b4ab7df0cb6bc74acd3b60f811dfec8fb8ad1a0
nasreddineMaizi <nasreddine1410@gmail.com>
2025-08-06 21:01:42 +0200

    add user registration with Email

--- book-network-backend/src/main/java/com/nasro/book/BookNetworkBackendApplication.java
+++ book-network-backend/src/main/java/com/nasro/book/BookNetworkBackendApplication.java
@@ -1,15 +1,32 @@
 package com.nasro.book;
 
+import com.nasro.book.role.Role;
+import com.nasro.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class BookNetworkBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookNetworkBackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if(roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
+    }
 }
--- book-network-backend/src/main/java/com/nasro/book/auth/AuthenticationService.java
+++ book-network-backend/src/main/java/com/nasro/book/auth/AuthenticationService.java
@@ -1,9 +1,84 @@
 package com.nasro.book.auth;
 
+import com.nasro.book.email.EmailService;
+import com.nasro.book.email.EmailTemplateName;
+import com.nasro.book.role.RoleRepository;
+import com.nasro.book.user.Token;
+import com.nasro.book.user.TokenRepository;
+import com.nasro.book.user.User;
+import com.nasro.book.user.UserRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
-    public void register(RegistrationRequest request) {
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    public void register(RegistrationRequest request) throws MessagingException {
+        var userRole = roleRepository.findByName("USER")
+                //todo - better exception Handling
+                .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
+        var user = User.builder()
+                .firstname(request.firstname())
+                .lastname(request.lastname())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
+        emailService.sendEmail(
+                user.getEmail(),
+                user.fullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account Activation "
+
+        );
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        //generate a token
+        String generatedToken = generateAtivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateAtivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder =new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(secureRandom.nextInt(characters.length())));
+        }
+        return codeBuilder.toString();
     }
 }
